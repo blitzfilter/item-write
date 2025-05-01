2f2dcb024b3bb274fe761e2b2661b54b9e22681e
bruderj15 <julian.bruder@outlook.com>
2025-05-01 21:30:01 +0200

    main: write a single item batch

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,13 @@ use aws_sdk_dynamodb::error::SdkError;
 use aws_sdk_dynamodb::operation::batch_write_item::{BatchWriteItemError, BatchWriteItemOutput};
 use aws_sdk_dynamodb::operation::put_item::{PutItemError, PutItemOutput};
 use aws_sdk_dynamodb::types::{PutRequest, WriteRequest};
-use futures::future::{join_all};
+use futures::future::join_all;
 use item_core::item_model::ItemModel;
 use serde_dynamo::aws_sdk_dynamodb_1::to_item;
 
 const MAX_BATCH_SIZE: usize = 25;
 
+/// Writes a single item to the DynamoDB abstracted by the given client.
 pub async fn write_item(
     item: &ItemModel,
     ddb_client: &Client,
@@ -22,7 +23,9 @@ pub async fn write_item(
         .await
 }
 
-pub async fn write_items<'items>(
+/// Writes multiple items by splitting them into batches and writes these batches
+/// to the DynamoDB abstracted by the given client.
+pub async fn write_items(
     items: &[ItemModel],
     ddb_client: &Client,
 ) -> Vec<Result<BatchWriteItemOutput, SdkError<BatchWriteItemError, HttpResponse>>> {
@@ -45,4 +48,23 @@ pub async fn write_items<'items>(
         .collect();
 
     join_all(reqs).await
+}
+
+/// Puts multiple items in a single batch and writes it to the DynamoDB abstracted by the given client.
+pub async fn write_item_batch(
+    items: &[ItemModel],
+    ddb_client: &Client,
+) -> Result<BatchWriteItemOutput, SdkError<BatchWriteItemError, HttpResponse>> {
+    let reqs = items
+        .iter()
+        .map(|item_diff| to_item(item_diff).ok())
+        .map(|payload| PutRequest::builder().set_item(payload).build().ok())
+        .map(|req| WriteRequest::builder().set_put_request(req).build())
+        .collect();
+
+    ddb_client
+        .batch_write_item()
+        .request_items("items", reqs)
+        .send()
+        .await
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -2,7 +2,7 @@ use item_core::item_model::ItemModel;
 use item_read::item::{
     get_item_events_by_item_id_sort_latest, get_item_events_by_item_id_sort_oldest,
 };
-use item_write::{write_item, write_items};
+use item_write::{write_item, write_item_batch, write_items};
 use test_api::generator::Generator;
 use test_api::localstack::get_dynamodb_client;
 use test_api::test_api_macros::blitzfilter_dynamodb_test;
@@ -55,4 +55,24 @@ async fn should_write_new_items_when_above_max_batch_size() {
     write_ress
         .iter()
         .for_each(|write_res| assert!(write_res.is_ok()))
+}
+
+#[blitzfilter_dynamodb_test]
+async fn should_create_items_when_write_new_item_batch() {
+    let items = ItemModel::generate_n::<25>();
+
+    let client = get_dynamodb_client().await;
+    let write_res = write_item_batch(&items, client).await;
+    assert!(write_res.is_ok());
+
+    // verify write
+    let read_items_res = try_join!(
+        get_item_events_by_item_id_sort_oldest(&items[0].item_id, client),
+        get_item_events_by_item_id_sort_oldest(&items[1].item_id, client),
+    );
+    assert!(read_items_res.is_ok());
+
+    let (read_items1, read_items2) = read_items_res.unwrap();
+    assert_eq!(*read_items1.get(0).unwrap(), items[0]);
+    assert_eq!(*read_items2.get(0).unwrap(), items[1]);
 }
