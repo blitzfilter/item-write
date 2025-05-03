1fce227b2660e5c3cb44a0c20e6017ee028b51d7
bruderj15 <julian.bruder@outlook.com>
2025-05-03 13:37:00 +0200

    main: logging via `tracing`

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@ use aws_sdk_dynamodb::config::http::HttpResponse;
 use aws_sdk_dynamodb::error::SdkError;
 use aws_sdk_dynamodb::operation::batch_write_item::{BatchWriteItemError, BatchWriteItemOutput};
 use aws_sdk_dynamodb::operation::put_item::{PutItemError, PutItemOutput};
-use aws_sdk_dynamodb::types::{PutRequest, WriteRequest};
+use aws_sdk_dynamodb::types::{AttributeValue, PutRequest, WriteRequest};
 use futures::future::join_all;
 use item_core::item_model::ItemModel;
 use serde_dynamo::aws_sdk_dynamodb_1::to_item;
+use std::collections::HashMap;
+use tracing::error;
 
-const MAX_BATCH_SIZE: usize = 25;
+pub const MAX_BATCH_SIZE: usize = 25;
 
 /// Writes a single item to the DynamoDB abstracted by the given client.
 pub async fn write_item(
@@ -18,7 +20,7 @@ pub async fn write_item(
     ddb_client
         .put_item()
         .table_name("items")
-        .set_item(to_item(item).ok())
+        .set_item(handle_to_item(item))
         .send()
         .await
 }
@@ -34,9 +36,14 @@ pub async fn write_items(
         .map(|chunk| {
             chunk
                 .iter()
-                .map(|item_diff| to_item(item_diff).ok())
-                .map(|payload| PutRequest::builder().set_item(payload).build().ok())
-                .map(|req| WriteRequest::builder().set_put_request(req).build())
+                .map(|item_diff| handle_to_item(item_diff))
+                .map(|payload| {
+                    PutRequest::builder()
+                        .set_item(payload)
+                        .build()
+                        .expect("shouldn't fail because because 'item' has been set.")
+                })
+                .map(|req| WriteRequest::builder().set_put_request(Some(req)).build())
                 .collect()
         })
         .map(|reqs| {
@@ -57,9 +64,14 @@ pub async fn write_item_batch(
 ) -> Result<BatchWriteItemOutput, SdkError<BatchWriteItemError, HttpResponse>> {
     let reqs = items
         .iter()
-        .map(|item_diff| to_item(item_diff).ok())
-        .map(|payload| PutRequest::builder().set_item(payload).build().ok())
-        .map(|req| WriteRequest::builder().set_put_request(req).build())
+        .map(|item_diff| handle_to_item(item_diff))
+        .map(|payload| {
+            PutRequest::builder()
+                .set_item(payload)
+                .build()
+                .expect("shouldn't fail because because 'item' has been set.")
+        })
+        .map(|req| WriteRequest::builder().set_put_request(Some(req)).build())
         .collect();
 
     ddb_client
@@ -67,4 +79,17 @@ pub async fn write_item_batch(
         .request_items("items", reqs)
         .send()
         .await
+}
+
+fn handle_to_item(item: &ItemModel) -> Option<HashMap<String, AttributeValue>> {
+    match to_item(item) {
+        Ok(payload) => Some(payload),
+        Err(e) => {
+            error!(
+                "Failed to serialize ItemModel. ItemModel: '{:?}'. Error: {e}",
+                item
+            );
+            None
+        }
+    }
 }
